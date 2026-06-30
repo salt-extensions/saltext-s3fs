b03cd7ccd295444d990cccd4f8e2f47bc90242a4
durrendal <durrendal@lambdacreate.com>
2026-06-30 12:16:14 +0000

    Add suppot for multi env buckets
    Addresses:
    https://github.com/saltstack/salt/issues/68335
    
    With patchset, including defensive changes Twangboy requested:
    https://github.com/saltstack/salt/pull/68452

--- src/saltext/s3fs/fileserver/s3fs.py
+++ src/saltext/s3fs/fileserver/s3fs.py
@@ -91,7 +91,6 @@ import datetime
 import logging
 import os
 import pickle
-import pprint
 import time
 import urllib.parse
 
@@ -100,7 +99,6 @@ import salt.modules
 import salt.utils.files
 import salt.utils.gzip_util
 import salt.utils.hashutils
-import salt.utils.s3
 import salt.utils.versions
 
 log = logging.getLogger(__name__)
@@ -134,12 +132,16 @@ def update():
             for bucket_files in _find_files(env_meta):
                 for bucket, files in bucket_files.items():
                     for file_path in files:
-                        cached_file_path = _get_cached_file_name(bucket, saltenv, file_path)
+                        cached_file_path = _get_cached_file_name(
+                            bucket, saltenv, file_path
+                        )
 
                         log.debug("%s - %s : %s", bucket, saltenv, file_path)
 
                         # load the file from S3 if it's not in the cache or it's old
-                        _get_file_from_s3(metadata, saltenv, bucket, file_path, cached_file_path)
+                        _get_file_from_s3(
+                            metadata, saltenv, bucket, file_path, cached_file_path
+                        )
 
         log.info("Sync local cache from S3 completed.")
 
@@ -203,7 +205,9 @@ def file_hash(load, fnd):
     if "path" not in fnd or "bucket" not in fnd or not fnd["path"]:
         return ret
 
-    cached_file_path = _get_cached_file_name(fnd["bucket"], load["saltenv"], fnd["path"])
+    cached_file_path = _get_cached_file_name(
+        fnd["bucket"], load["saltenv"], fnd["path"]
+    )
 
     if os.path.isfile(cached_file_path):
         ret["hash_type"] = S3_HASH_TYPE
@@ -231,7 +235,9 @@ def serve_file(load, fnd):
     gzip = load.get("gzip", None)
 
     # get the saltenv/path file from the cache
-    cached_file_path = _get_cached_file_name(fnd["bucket"], load["saltenv"], fnd["path"])
+    cached_file_path = _get_cached_file_name(
+        fnd["bucket"], load["saltenv"], fnd["path"]
+    )
 
     ret["dest"] = _trim_env_off_path([fnd["path"]], load["saltenv"])[0]
 
@@ -273,14 +279,14 @@ def file_list(load):
     return ret
 
 
-# def file_list_emptydirs(load):
-#    """
-#    Return a list of all empty directories on the master
-#    """
-#    # TODO - implement this
-#    _init()
-#
-#    return []
+def file_list_emptydirs(load):
+    """
+    Return a list of all empty directories on the master
+    """
+    # TODO - implement this
+    _init()
+
+    return []
 
 
 def dir_list(load):
@@ -325,7 +331,9 @@ def _get_s3_key():
     kms_keyid = __opts__["aws.kmw.keyid"] if "aws.kms.keyid" in __opts__ else None
     location = __opts__["s3.location"] if "s3.location" in __opts__ else None
     path_style = __opts__["s3.path_style"] if "s3.path_style" in __opts__ else None
-    https_enable = __opts__["s3.https_enable"] if "s3.https_enable" in __opts__ else None
+    https_enable = (
+        __opts__["s3.https_enable"] if "s3.https_enable" in __opts__ else None
+    )
 
     return (
         key,
@@ -402,7 +410,7 @@ def _refresh_buckets_cache_file(cache_file):
         keyid,
         service_url,
         verify_ssl,
-        _,
+        kms_keyid,
         location,
         path_style,
         https_enable,
@@ -414,7 +422,7 @@ def _refresh_buckets_cache_file(cache_file):
     def __get_s3_meta(bucket, key=key, keyid=keyid):
         ret, marker = [], ""
         while True:
-            tmp = salt.utils.s3.query(
+            tmp = __utils__["s3.query"](
                 key=key,
                 keyid=keyid,
                 kms_keyid=keyid,
@@ -433,7 +441,9 @@ def _refresh_buckets_cache_file(cache_file):
                     break
                 headers.append(header)
             ret.extend(tmp)
-            if all(header.get("IsTruncated", "false") == "false" for header in headers):
+            if all(
+                [header.get("IsTruncated", "false") == "false" for header in headers]
+            ):
                 break
             marker = tmp[-1]["Key"]
         return ret
@@ -478,8 +488,11 @@ def _refresh_buckets_cache_file(cache_file):
                                 bucket_name,
                             )
                             continue
-                        log.warning("S3 Error! Do you have any files in your S3 bucket?")
-                        return {}
+                        else:
+                            log.warning(
+                                "S3 Error! Do you have any files in your S3 bucket?"
+                            )
+                            return {}
 
             metadata[saltenv] = bucket_files_list
 
@@ -519,8 +532,11 @@ def _refresh_buckets_cache_file(cache_file):
                             bucket_name,
                         )
                         continue
-                    log.warning("S3 Error! Do you have any files in your S3 bucket?")
-                    return {}
+                    else:
+                        log.warning(
+                            "S3 Error! Do you have any files in your S3 bucket?"
+                        )
+                        return {}
 
             environments = [(os.path.dirname(k["Key"]).split("/", 1))[0] for k in files]
             environments = set(environments)
@@ -575,13 +591,23 @@ def _prune_deleted_files(metadata):
                     roots.add(root)
 
             for meta in env_data:
-                cached_files.add(meta["Key"])
+                if "Key" in meta:
+                    cached_files.add(meta["Key"])
+                else:
+                    # Handle the nested structure for multi-environment buckets
+                    for objects in meta.values():
+                        if isinstance(objects, list):
+                            for obj in objects:
+                                if isinstance(obj, dict) and "Key" in obj:
+                                    cached_files.add(obj["Key"])
 
     if log.isEnabledFor(logging.DEBUG):
+        import pprint
+
         log.debug("cached file list:\n%s", pprint.pformat(cached_files))
 
     for root in roots:
-        for base, _, files in os.walk(root):
+        for base, dirs, files in os.walk(root):
             for file_name in files:
                 path = os.path.join(base, file_name)
                 relpath = os.path.relpath(path, root)
@@ -751,7 +777,7 @@ def _get_file_from_s3(metadata, saltenv, bucket_name, path, cached_file_path):
         keyid,
         service_url,
         verify_ssl,
-        _,
+        kms_keyid,
         location,
         path_style,
         https_enable,
@@ -765,7 +791,9 @@ def _get_file_from_s3(metadata, saltenv, bucket_name, path, cached_file_path):
 
             if file_etag.find("-") == -1:
                 file_md5 = file_etag
-                cached_md5 = salt.utils.hashutils.get_hash(cached_file_path, S3_HASH_TYPE)
+                cached_md5 = salt.utils.hashutils.get_hash(
+                    cached_file_path, S3_HASH_TYPE
+                )
 
                 # hashes match we have a cache hit
                 if cached_md5 == file_md5:
@@ -775,7 +803,9 @@ def _get_file_from_s3(metadata, saltenv, bucket_name, path, cached_file_path):
             else:
                 cached_file_stat = os.stat(cached_file_path)
                 cached_file_size = cached_file_stat.st_size
-                cached_file_mtime = datetime.datetime.fromtimestamp(cached_file_stat.st_mtime)
+                cached_file_mtime = datetime.datetime.fromtimestamp(
+                    cached_file_stat.st_mtime
+                )
 
                 cached_file_lastmod = datetime.datetime.strptime(
                     file_meta["LastModified"], "%Y-%m-%dT%H:%M:%S.%fZ"
@@ -789,7 +819,7 @@ def _get_file_from_s3(metadata, saltenv, bucket_name, path, cached_file_path):
                         "cached file mtime later than metadata last "
                         "modification time."
                     )
-                    ret = salt.utils.s3.query(
+                    ret = __utils__["s3.query"](
                         key=key,
                         keyid=keyid,
                         kms_keyid=keyid,
@@ -828,7 +858,7 @@ def _get_file_from_s3(metadata, saltenv, bucket_name, path, cached_file_path):
                             return
 
     # ... or get the file from S3
-    salt.utils.s3.query(
+    __utils__["s3.query"](
         key=key,
         keyid=keyid,
         kms_keyid=keyid,
--- tests/unit/fileserver/test_s3fs.py
+++ tests/unit/fileserver/test_s3fs.py
@@ -1,22 +1,31 @@
 import os
 
-import boto3
 import pytest
-import salt.utils.files
-import salt.utils.hashutils
-import salt.utils.s3
 import yaml
 
 # moto must be imported before boto3
-from moto import mock_aws
+try:
+    import boto3
+    from moto import mock_aws
 
-from saltext.s3fs.fileserver import s3fs
+    HAS_BOTO = True
+except ImportError:
+    HAS_BOTO = False
+
+import salt.fileserver.s3fs as s3fs
+import salt.utils.s3
+from tests.support.mock import patch
+
+pytestmark = [
+    pytest.mark.skipif(not HAS_BOTO, reason="Missing library moto or boto3"),
+    pytest.mark.windows_whitelisted,
+]
 
 
 @pytest.fixture
 def bucket():
-    """Return a test bucket name."""
-    return "mybucket"
+    with mock_aws():
+        yield "mybucket"
 
 
 @pytest.fixture(scope="module")
@@ -30,11 +39,23 @@ def aws_creds():
 
 
 @pytest.fixture(scope="function")
+def configure_loader_modules(tmp_path, bucket):
+    opts = {
+        "cachedir": tmp_path,
+        "s3.buckets": {"base": [bucket]},
+        "s3.location": "us-east-1",
+        "s3.s3_cache_expire": -1,
+    }
+    utils = {"s3.query": salt.utils.s3.query}
+
+    yield {s3fs: {"__opts__": opts, "__utils__": utils}}
+
+
+@pytest.fixture(scope="function")
 def s3(bucket, aws_creds):
-    with mock_aws():
-        conn = boto3.client("s3", **aws_creds)
-        conn.create_bucket(Bucket=bucket)
-        yield conn
+    conn = boto3.client("s3", **aws_creds)
+    conn.create_bucket(Bucket=bucket)
+    return conn
 
 
 def make_keys(bucket, conn, keys):
@@ -60,17 +81,9 @@ def verify_cache(bucket, expected):
             assert correct_content == content
 
 
-@mock_aws
+@pytest.mark.skip_on_fips_enabled_platform
 def test_update(bucket, s3):
     """Tests that files get downloaded from s3 to the local cache."""
-
-    # Set up __opts__ directly on the module
-    s3fs.__opts__ = {
-        "cachedir": "/tmp/test_cache",
-        "s3.buckets": {"base": [bucket]},
-        "s3.location": "us-east-1",
-        "s3.s3_cache_expire": -1,
-    }
 
     keys = {
         "top.sls": {"content": yaml.dump({"base": {"*": ["foo"]}})},
@@ -125,17 +138,9 @@ def test_update(bucket, s3):
     assert not os.path.exists(files_dir)
 
 
-@mock_aws
+@pytest.mark.skip_on_fips_enabled_platform
 def test_s3_hash(bucket, s3):
     """Verifies that s3fs hashes files correctly."""
-
-    # Set up __opts__ directly on the module
-    s3fs.__opts__ = {
-        "cachedir": "/tmp/test_cache",
-        "s3.buckets": {"base": [bucket]},
-        "s3.location": "us-east-1",
-        "s3.s3_cache_expire": -1,
-    }
 
     keys = {
         "top.sls": {"content": yaml.dump({"base": {"*": ["foo"]}})},
@@ -148,7 +153,9 @@ def test_s3_hash(bucket, s3):
 
     for key, item in keys.items():
         cached_file_path = s3fs._get_cached_file_name(bucket, "base", key)
-        item["hash"] = salt.utils.hashutils.get_hash(cached_file_path, s3fs.S3_HASH_TYPE)
+        item["hash"] = salt.utils.hashutils.get_hash(
+            cached_file_path, s3fs.S3_HASH_TYPE
+        )
         item["cached_file_path"] = cached_file_path
 
     load = {"saltenv": "base"}
@@ -161,15 +168,8 @@ def test_s3_hash(bucket, s3):
         assert item["hash"] == actual_hash["hsum"]
 
 
+@pytest.mark.skip_on_fips_enabled_platform
 def test_cache_round_trip(bucket):
-    # Set up __opts__ directly on the module
-    s3fs.__opts__ = {
-        "cachedir": "/tmp/test_cache",
-        "s3.buckets": {"base": [bucket]},
-        "s3.location": "us-east-1",
-        "s3.s3_cache_expire": -1,
-    }
-
     metadata = {"foo": "bar"}
     cache_file = s3fs._get_buckets_cache_filename()
     s3fs._write_buckets_cache_file(metadata, cache_file)
@@ -180,3 +180,96 @@ def test_ignore_pickle_load_exceptions():
     #  TODO: parameterized test with patched pickle.load that raises the
     #  various allowable exception from _read_buckets_cache_file
     pass
+
+
+@pytest.mark.skip_on_fips_enabled_platform
+def test_prune_deleted_files_multiple_envs_per_bucket(bucket, s3):
+    """
+    Test that _prune_deleted_files does not raise KeyError in
+    multi-environment-per-bucket mode (issue #68335).
+
+    Prior to the fix the function read meta["Key"] off the
+    {bucket: [...]} dict it received as `meta`, raising
+    KeyError: 'Key'. The fix descends one more level
+    (meta.values() -> obj["Key"]) so the cached_files set is
+    populated correctly.
+    """
+
+    # Create test files in S3 with the env as the first path component
+    # (this is how multi-env-per-bucket mode discovers environments).
+    keys = {
+        "base/test1.sls": {"content": "test1 content"},
+        "base/test2.sls": {"content": "test2 content"},
+        "dev/test3.sls": {"content": "test3 content"},
+    }
+    make_keys(bucket, s3, keys)
+
+    # Override s3.buckets to a list, which switches s3fs into
+    # multi-env-per-bucket mode (vs. the dict form set by the fixture).
+    with patch.dict(s3fs.__opts__, {"s3.buckets": [bucket]}):
+        # Populate the cache so update() lays down real files.
+        s3fs.update()
+
+        # In multi-env mode the env prefix is part of the S3 key, so the
+        # cached path is <cachedir>/<env>/<bucket>/<env>/<filename>.
+        for key in keys:
+            env = key.split("/", 1)[0]
+            cache_file = s3fs._get_cached_file_name(bucket, env, key)
+            assert os.path.exists(cache_file)
+
+        # Hand-built metadata in the shape s3fs._init() produces for
+        # multi-env-per-bucket mode: {saltenv: [{bucket_name: [file_meta]}]}.
+        # Prior to the fix this exact structure caused KeyError: 'Key'.
+        metadata = {
+            "base": [
+                {
+                    bucket: [
+                        {"Key": "base/test1.sls"},
+                        {"Key": "base/test2.sls"},
+                    ]
+                }
+            ],
+            "dev": [{bucket: [{"Key": "dev/test3.sls"}]}],
+        }
+
+        # The fix is verified by this call returning without raising.
+        s3fs._prune_deleted_files(metadata)
+
+
+@pytest.mark.skip_on_fips_enabled_platform
+def test_prune_deleted_files_single_env_per_bucket(bucket, s3):
+    """Test that _prune_deleted_files works correctly with single environment per bucket."""
+
+    # The configure_loader_modules fixture already sets s3.buckets to
+    # {"base": [bucket]} (dict form = env-per-bucket mode).
+    keys = {
+        "test1.sls": {"content": "test1 content"},
+        "test2.sls": {"content": "test2 content"},
+    }
+    make_keys(bucket, s3, keys)
+
+    # Initial update to populate cache
+    s3fs.update()
+
+    # Verify files are cached
+    for key in keys:
+        cache_file = s3fs._get_cached_file_name(bucket, "base", key)
+        assert os.path.exists(cache_file)
+
+    # Delete one file from S3
+    s3.delete_object(Bucket=bucket, Key="test1.sls")
+    del keys["test1.sls"]
+
+    # Metadata in the shape s3fs._init() produces for env-per-bucket mode.
+    metadata = {"base": [{bucket: [{"Key": "test2.sls"}]}]}
+
+    # Call _prune_deleted_files directly
+    s3fs._prune_deleted_files(metadata)
+
+    # Verify that deleted file was removed from cache
+    deleted_cache_file = s3fs._get_cached_file_name(bucket, "base", "test1.sls")
+    assert not os.path.exists(deleted_cache_file)
+
+    # Verify that remaining file still exists
+    remaining_cache_file = s3fs._get_cached_file_name(bucket, "base", "test2.sls")
+    assert os.path.exists(remaining_cache_file)
